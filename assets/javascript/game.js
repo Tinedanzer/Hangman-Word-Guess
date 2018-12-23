07decee782399d757beef952dfe1611fcc4719db
Tinedanzer <johnhillallen@gmail.com>
2018-12-23 00:10:46 -0600

    duplicate letter check

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,18 +11,22 @@ document.onkeyup = function(event) {
     
     const textnode =document.createTextNode(event.key);
     // node.append(textnode);
+    let x=event.key;
+    // console.log(event.key);
     for (let i = 0; i < guessJs.length; i++) {
-        if(event.key===guessJs[i]){
-             alert('You have already chosen this letter, try being better!');
-             break;
+        console.log(guessJs[i]);
+        if(x===guessJs[i]){
+             alert('You have already chosen this letter, try being better!');       
         }
     else {
     $("#guessed").append(spacing);
     $("#guessed").append(textnode);
+    
+    }
+    
+//    exits for loop
+    }
+    // need to place this another spot in order for no duplicate letters in guessed letter array
     guessJs.push(event.key);
-    }
-   
-    }
-    // document.getElementById("guessed").appendChild(textnode);
 }
 
