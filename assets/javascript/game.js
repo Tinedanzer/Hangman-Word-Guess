7887c5d4b8bfd3524c27e64ef470ff0ecbf212d6
Tinedanzer <johnhillallen@gmail.com>
2018-12-23 03:04:17 -0600

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,29 +4,31 @@ const guessJs=[''];
 function randomSelection(){
     return Math.floor(Math.random()*currentWord.length)+1;
 }
+function pushKey(){
+    guessJs.push(event.key);
+}
 // returns a number of letters based on the random word selected:
 // currentWord[randomSelection()].length
 document.onkeyup = function(event) {
     // var node =document.createElement("li");
-    
-    const textnode =document.createTextNode(event.key);
+    const x= event.key;
+    const textnode =document.createTextNode(x);
     // node.append(textnode);
-    let x=event.key;
-    // console.log(event.key);
+    // console.log(x);
     for (let i = 0; i < guessJs.length; i++) {
-        console.log(guessJs[i]);
-        if(x===guessJs[i]){
-             alert('You have already chosen this letter, try being better!');       
+        // .every after guessJs.every checks all of the array at once. can be used in the parameter
+        if(x !== guessJs[i] && x !== guessJs[i+1] && x!==guessJs[i+2]&&x!==guessJs[i+3]){
+            $("#guessed").append(spacing);
+            $("#guessed").append(textnode);    
+            pushKey();
+            break;
         }
-    else {
-    $("#guessed").append(spacing);
-    $("#guessed").append(textnode);
-    
-    }
-    
+        else if(x === guessJs[i]){
+            alert('You have already chosen this letter, try being better!');   
+            break;
+        }
+        else{};
 //    exits for loop
-    }
-    // need to place this another spot in order for no duplicate letters in guessed letter array
-    guessJs.push(event.key);
+    };
 }
 
