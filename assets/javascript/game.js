f3d43b1352ba3ac4c8226d820f96f5472eeb4961
Tinedanzer <johnhillallen@gmail.com>
2018-12-22 23:12:58 -0600

    added math random, and did some conditional checks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,28 @@
-
-// document.addEventListener("keyup", myFunction);
-// document.onkeyup = function(event) {
-//     $("#guessed").append(event);
-// }
-
-// function myFunction() {
-//   const x = document.onkeyup;
-//   x = x.value.toLowerCase();
-//   $("#guessed").append(x);
-// }
 const spacing= " ";
-// document.addEventListener("keyup", myFunction);
-
-// function myFunction(event) {
-//     $("#guessed").append(event);
-//   var x = document.getElementById("fname");
-//   x.value = x.value.toLowerCase();
-// }
-
+const currentWord=['cheer','grumpy', 'funshine','tenderheart', 'share','surprise'];
+const guessJs=[''];
+function randomSelection(){
+    return Math.floor(Math.random()*currentWord.length)+1;
+}
+// returns a number of letters based on the random word selected:
+// currentWord[randomSelection()].length
 document.onkeyup = function(event) {
     // var node =document.createElement("li");
+    
     const textnode =document.createTextNode(event.key);
     // node.append(textnode);
+    for (let i = 0; i < guessJs.length; i++) {
+        if(event.key===guessJs[i]){
+             alert('You have already chosen this letter, try being better!');
+             break;
+        }
+    else {
     $("#guessed").append(spacing);
     $("#guessed").append(textnode);
+    guessJs.push(event.key);
+    }
+   
+    }
     // document.getElementById("guessed").appendChild(textnode);
 }
 
-function randomSelec(){
-    return Math.floor(Math.random()*currentWord.length)+1;
-}
